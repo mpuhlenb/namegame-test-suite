df2209e9da1c587d1e5b374b2819c8cfadd9ad85
Morris Uhlenbrauck <mpuhlenb@yahoo.com>
2019-10-21 09:12:59 -0400

    Cleaned up code formatting added couple variables

--- cypress/integration/util.js
+++ cypress/integration/util.js
@@ -1,29 +1,43 @@
 export const createASimpleStreak = (streak) => {
-    // Just speeding up setting up streak environment
 
+    // Just speeding up setting up streak environment
     for (let i = 0; i < streak; i++) {
+
         clickCorrectPhoto()
 
     }
 }
 
 export const clickCorrectPhoto = () => {
+
     //Make sure we have not tried to select a photo if a correct one is shown
     if (!checkIfCorrectPhotoLabelNotExist()) {
+
         // Get the data attribute from the name
-        cy.get('#name').attribute('data-n').then((datan) => {
+        cy.get('#name')
+        .attribute('data-n')
+        .then((datan) => {
+
             // Use attribute as index of correct
-            cy.get('.photo').children('div.shade').eq(datan).click()
+            cy.get('.photo')
+            .children('div.shade')
+            .eq(datan)
+            .click()
+
         })
     }
 }
 
 export const clickWrongPhoto = () => {
-    var i = 0
+
     //Make sure we have not tried to select a photo if a correct one is shown
     if (!checkIfCorrectPhotoLabelNotExist()) {
+
         // Get the data attribute from name to ensure we do not select a correct photo
-        cy.get('#name').attribute('data-n').then((datan) => {
+        cy.get('#name')
+        .attribute('data-n')
+        .then((datan) => {
+
             //Make sure we do not select a correct guess add indexer to datan
             switch (Number(datan)) {
                 case 4:
@@ -33,15 +47,23 @@ export const clickWrongPhoto = () => {
                     var indexer = 1
                     break;
             }
-            cy.get('.photo').eq(Number(datan) + indexer).should('not.have.class', 'wrong').click()
+
+            cy.get('.photo')
+                .eq(Number(datan) + indexer)
+                .should('not.have.class', 'wrong')
+                .click()
         })
     }
 }
 
 const checkIfCorrectPhotoLabelNotExist = () => {
     //Quick method to avoid clicking while page reloads, possible defect
+
     try {
-        cy.get('.photo.correct').should('not.exist')
+
+        cy.get('.photo.correct')
+        .should('not.exist')
+        
     } catch (error) {
         return false
     }
